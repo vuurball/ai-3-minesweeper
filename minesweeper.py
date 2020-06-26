c3a3a8732a02c9114e874ea29f165a22e7696740
vuurball <oneindigedroom@gmail.com>
2020-06-26 14:01:03 +0300

    finished

--- minesweeper.py
+++ minesweeper.py
@@ -1,6 +1,6 @@
 import itertools
 import random
-
+import copy
 
 class Minesweeper():
     """
@@ -13,6 +13,7 @@ class Minesweeper():
         self.height = height
         self.width = width
         self.mines = set()
+        self.mines_found = set()
 
         # Initialize an empty field with no mines
         self.board = []
@@ -29,9 +30,6 @@ class Minesweeper():
             if not self.board[i][j]:
                 self.mines.add((i, j))
                 self.board[i][j] = True
-
-        # At first, player has found no mines
-        self.mines_found = set()
 
     def print(self):
         """
@@ -105,27 +103,34 @@ class Sentence():
         """
         Returns the set of all cells in self.cells known to be mines.
         """
-        raise NotImplementedError
+        if len(self.cells) == self.count:
+            return self.cells # all cells in this sentence are mines
+        return None
 
     def known_safes(self):
         """
         Returns the set of all cells in self.cells known to be safe.
         """
-        raise NotImplementedError
+        if self.count == 0:
+            return self.cells # all cells in this sentence are safe
+        return None
 
     def mark_mine(self, cell):
         """
         Updates internal knowledge representation given the fact that
         a cell is known to be a mine.
         """
-        raise NotImplementedError
+        if cell in self.cells:
+            self.cells.remove(cell)
+            self.count -= 1
 
     def mark_safe(self, cell):
         """
         Updates internal knowledge representation given the fact that
         a cell is known to be safe.
         """
-        raise NotImplementedError
+        if cell in self.cells:
+            self.cells.remove(cell)
 
 
 class MinesweeperAI():
@@ -171,18 +176,48 @@ class MinesweeperAI():
         """
         Called when the Minesweeper board tells us, for a given
         safe cell, how many neighboring cells have mines in them.
-
-        This function should:
-            1) mark the cell as a move that has been made
-            2) mark the cell as safe
-            3) add a new sentence to the AI's knowledge base
-               based on the value of `cell` and `count`
-            4) mark any additional cells as safe or as mines
-               if it can be concluded based on the AI's knowledge base
-            5) add any new sentences to the AI's knowledge base
-               if they can be inferred from existing knowledge
         """
-        raise NotImplementedError
+        # This function should:
+        # 1) mark the cell as a move that has been made
+        self.moves_made.add(cell)
+        # 2) mark the cell as safe
+        self.mark_safe(cell)
+        # (*) remove this cell from self.safes (safe optional moves)
+        self.safes.remove(cell)
+        # 3) add a new sentence to the AI's knowledge base
+        # based on the value of `cell` and `count`
+        self.knowledge.append(Sentence(self.nearby_cells(cell), count))
+        # 4) mark any additional cells as safe or as mines
+        # if it can be concluded based on the AI's knowledge base
+        safe_cells = set()
+        mine_cells = set()     
+        for sentence in self.knowledge:
+            known_safe_cells = sentence.known_safes()
+            if known_safe_cells is not None:
+                safe_cells = safe_cells.union(known_safe_cells)
+            known_mine_cells = sentence.known_mines()
+            if known_mine_cells is not None:
+                mine_cells = mine_cells.union(known_mine_cells)
+        for safe_cell in safe_cells:
+            self.mark_safe(safe_cell)
+        for mine_cell in mine_cells:
+            self.mark_mine(mine_cell)
+        # 5) add any new sentences to the AI's knowledge base
+        # if they can be inferred from existing knowledge
+        for s1 in self.knowledge:
+            for s2 in self.knowledge:
+                if s1 != s2:
+                    if s1.cells.issubset(s2.cells):
+                        # s1 exists in s2, so we can remove s1 cells from s2 & decrese the count
+                        # replace s2 with shorter version we just recalculated
+                        self.knowledge.remove(s2)
+                        # sentences_to_delete.append(s2)
+                        self.knowledge.append(Sentence(s2.cells.symmetric_difference(s1.cells), s2.count - s1.count))
+        # remove empty sentences
+        for sentence in self.knowledge:
+            if len(sentence.cells) == 0:
+                self.knowledge.remove(sentence)
+
 
     def make_safe_move(self):
         """
@@ -193,7 +228,10 @@ class MinesweeperAI():
         This function may use the knowledge in self.mines, self.safes
         and self.moves_made, but should not modify any of those values.
         """
-        raise NotImplementedError
+        for safe_cell in sorted(self.safes):
+            if safe_cell not in self.moves_made:
+                return safe_cell
+        return None        
 
     def make_random_move(self):
         """
@@ -202,4 +240,28 @@ class MinesweeperAI():
             1) have not already been chosen, and
             2) are not known to be mines
         """
-        raise NotImplementedError
+        for i in range(self.height):
+            for j in range(self.width):
+                if (i, j) not in self.moves_made and (i, j) not in self.mines:
+                    return (i, j)
+        return None
+
+    def nearby_cells(self, cell):
+        """
+        Returns a set pf cells that are
+        within one row and column of a given cell,
+        not including the cell itself.
+        """
+        # set of nearby cells
+        near_cells = set()
+        # Loop over all cells within one row and column
+        for i in range(cell[0] - 1, cell[0] + 2):
+            for j in range(cell[1] - 1, cell[1] + 2):
+                # Ignore the cell itself
+                if (i, j) == cell:
+                    continue
+                # add neighbor if cell in bounds and is not "revealed" yet
+                if 0 <= i < self.height and 0 <= j < self.width and (i,j) not in self.moves_made:
+                    near_cells.add((i, j))
+        return near_cells
+   
